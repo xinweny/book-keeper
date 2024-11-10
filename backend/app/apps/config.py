ddffb36f6a9aec6a30439d0fb2936f3d405a9bf2
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-09 22:11:15 -0500

    add appconfig

---
+++ backend/app/apps/config.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class BookKeeperConfig(AppConfig):
+    name = 'app'
+    verbose_name = 'Book Keeper'
--- backend/app/urls.py
+++ backend/app/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
 
 from app.apps.urls import router
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    path(r'api/', include(router.urls)),
 ]
