70394784c09551673fa0bb42f798ec05fdcf4f31
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-10 02:35:23 -0500

    fix isbn field to isbn13 format

--- backend/app/apps/book/models.py
+++ backend/app/apps/book/models.py
@@ -1,11 +1,13 @@
 from django.db.models import Model, CharField, DateField, ForeignKey, CASCADE
+from django.core.validators import MinLengthValidator
 
 from app.apps.author.models import Author
 from app.apps.genre.models import Genre
 
+# Table should have been named "Inventory"
 class Book(Model):
     title: CharField = CharField(max_length=255)
     author: ForeignKey[Author] = ForeignKey(Author, on_delete=CASCADE)
     genre: ForeignKey[Genre] = ForeignKey(Genre, on_delete=CASCADE)
     publication_date: DateField = DateField()
-    isbn: CharField = CharField(unique=True, max_length=13)
+    isbn: CharField = CharField(unique=True, max_length=13, validators=[MinLengthValidator(13)])
