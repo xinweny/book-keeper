9cbdbdf11f401768ba48edb772130bba13aae00c
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-09 23:16:29 -0500

    remove trailing whitespace

--- backend/app/apps/book/views.py
+++ backend/app/apps/book/views.py
@@ -8,15 +8,15 @@ from app.apps.book.serializers import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+
     def list(self, request):
         serializer = self.get_serializer(self.get_queryset(), many=True)
-        
+
         return Response(data=serializer.data)
-    
+
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)   
-        serializer.save()  
-        
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
