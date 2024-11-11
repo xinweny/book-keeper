5f80664188e7ed2788b03ccefe87559f96ea2a4b
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-10 20:42:38 -0500

    switch to relative src for font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 import "@/styles/globals.css";
 
 const geistSans = localFont({
-  src: "@/assets/fonts/GeistVF.woff",
+  src: "../assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "@/assets/fonts/GeistMonoVF.woff",
+  src: "../assets/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
