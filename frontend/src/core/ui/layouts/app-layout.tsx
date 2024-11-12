d922455175ae948a45f222edaf4e626eadba5a32
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-12 15:55:38 -0500

    add suspense boundary

--- frontend/src/core/ui/layouts/app-layout.tsx
+++ frontend/src/core/ui/layouts/app-layout.tsx
@@ -1,6 +1,7 @@
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+import { Suspense } from 'react';
 
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
@@ -15,17 +16,19 @@ export function AppLayout({
   children,
 }: AppLayoutProps) {
   return (
-    <NuqsAdapter>
-      <QueryClientProvider client={queryClient}>
-        <div className="flex flex-col min-h-screen max-w-screen overflow-auto">
-          <Header />
-          <main className="flex-grow p-4">
-            <Toaster />
-            {children}
-          </main>
-          <Footer />
-        </div>
-      </QueryClientProvider>
-    </NuqsAdapter>
+    <Suspense>
+      <NuqsAdapter>
+        <QueryClientProvider client={queryClient}>
+          <div className="flex flex-col min-h-screen max-w-screen overflow-auto">
+            <Header />
+            <main className="flex-grow p-4">
+              <Toaster />
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </QueryClientProvider>
+      </NuqsAdapter>
+    </Suspense>
   );
 }
