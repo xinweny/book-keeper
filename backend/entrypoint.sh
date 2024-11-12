ced5335868879c8b8dedc50c9e1ed968f85f262b
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-12 15:55:33 -0500

    simplify dockerfiles without entrypoint.sh

--- backend/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$ENVIRONMENT" = "production" ]; then
-    exec poetry run gunicorn -c gunicorn.conf.py
-elif [ "$ENVIRONMENT" = "development" ]; then
-    exec poetry run python manage.py runserver 0.0.0.0:8000
-else
-    echo "ENVIRONMENT variable is not set"
-fi
