a846301f280d9cdc749ffd5fa64fb3d922fdc091
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-12 17:28:09 -0500

    revert nextjs config to export for static page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Static export for simple single-page application
-  experimental: {
-    outputStandalone: true,
-  },
 };
 
 export default nextConfig;
