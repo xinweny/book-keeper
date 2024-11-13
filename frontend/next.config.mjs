3dd240d1c426527ac61a3e51088b48ef26712b36
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-13 14:07:29 -0500

    add manual pages setup in next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Static export for simple single-page application
-  assetPrefix: '',
+  basePath: '/book-keeper',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
