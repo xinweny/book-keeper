64f3bd8143eec5eb99f22ac811b62f3dd0d209f1
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-13 12:23:37 -0500

    add base path to env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Static export for simple single-page application
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export default nextConfig;
