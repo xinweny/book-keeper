e68950c3e9beadf2069606ba912b515273b7ab21
Xin Wen Yap <yapxinwen1608@gmail.com>
2024-11-13 13:32:15 -0500

    add relative path config for static asset

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Static export for simple single-page application
+  assetPrefix: '',
 };
 
 export default nextConfig;
